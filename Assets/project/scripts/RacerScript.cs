5977d08b7467522cfe55509349b27ba101df315f
Leobold08 <leonivakoski1@gmail.com>
2025-01-31 12:04:33 +0200

    checkpoint

---
+++ Assets/project/scripts/RacerScript.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RacerScript : MonoBehaviour
+{
+
+    public float laptime;
+    public float besttime = 0;
+    private bool startTimer = false;
+
+    public TextMeshProUGUI Ltime;
+    public TextMeshProUGUI Btime;
+
+    private bool checkpoint1 = false;
+    private bool checkpoint2 = false;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ltime.text = "Time: " + laptime.ToString("F2");
+
+        if(transform.position.y < 0)
+        {
+            ResetPosition();
+            GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            startTimer = false;
+            laptime = 0;
+        }
+
+
+        if(startTimer == true) 
+        {
+            laptime = laptime + Time.deltaTime;
+
+        }
+    }
+
+    void ResetPosition()
+    {
+        transform.position = new Vector3(255, 0.0015f, 845);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "StartFinish")
+        {
+            if (startTimer == false)
+            {
+                startTimer = true;
+                laptime = 0;
+                checkpoint1 = false;
+                checkpoint2 = false;
+            }
+
+
+            if(checkpoint1 == true && checkpoint2 == true)
+            {
+                startTimer = false;
+
+                if(besttime == 0)
+                {
+                    besttime = laptime;
+                }
+                if(laptime<besttime)
+                {
+                    besttime = laptime;
+                }
+
+                Btime.text = "Best: " + besttime.ToString("F2");
+            }
+            
+        }
+
+
+        if(other.gameObject.name == "checkpoint1")
+        {
+            Debug.Log("chek1");
+            checkpoint1 = true;
+        }
+
+        if(other.gameObject.name == "checkpoint2")
+        {
+            Debug.Log("chek2");
+            checkpoint2 = true;
+        }
+
+    }
+}
