44800a00cf8fd6b6de1489ecf2a82689b2210514
Leo Robert Valtteri Nivakoski <s4nile00@students.osao.fi>
2025-02-03 10:57:09 +0200

    spped

---
+++ Assets/project/scripts/SpeedMeter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeedMeter : MonoBehaviour
+{
+    public Rigidbody target;
+
+    public float maxSpeed = 0.0f;
+
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
+
+    private float speed;
+
+    [Header("UI")]
+    public TextMeshProUGUI speedLabel;
+    public RectTransform arrow;
+
+    private void Update()
+    {
+        speed = target.linearVelocity.magnitude * 3.6f;
+
+        if (speedLabel != null)
+            speedLabel.text = ((int)speed) + " km/h";
+        if (arrow != null)
+            arrow.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed / maxSpeed));
+    }
+}
