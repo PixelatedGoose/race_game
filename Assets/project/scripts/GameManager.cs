561ab54ff395a7abfb9902571c2df8848d5cb53a
Vizl87 <ojantakanenaleksi@gmail.com>
2025-02-24 12:35:04 +0200

    Score system

--- Assets/project/scripts/GameManager.cs
+++ Assets/project/scripts/GameManager.cs
@@ -1,11 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using TMPro;
+
+
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    private void Awake()
+    private int score;
+
+    private bool isAddingPoints = false;
+    
+    public TextMeshProUGUI Score;
+
+    void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    public void AddPoints()
+    {
+        if (!isAddingPoints)  
+        {
+            StartCoroutine(IncrementScoreWithDelay());
+        }
+    }
+
+    private IEnumerator IncrementScoreWithDelay()
+    {
+        isAddingPoints = true;   
+
+        while (isAddingPoints)   
+        {
+            yield return new WaitForSeconds(1f);   
+            score += 100;   
+            Score.text = "Points: " + score.ToString();               
+
+        }
+    }
+
+    public void StopAddingPoints()
+    {
+        isAddingPoints = false;  
     }
 }
