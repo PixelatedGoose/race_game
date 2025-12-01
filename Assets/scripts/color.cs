4f2c2a1d8198497cee01497fa234cc1f7e16a8bd
ThatOneGuyC <thatoneguyextra@gmail.com>
2025-12-01 13:54:11 +0200

    crash fix + remove framerate

--- Assets/scripts/color.cs
+++ Assets/scripts/color.cs
@@ -1,103 +1,95 @@
-// using UnityEngine;
+using UnityEngine;
 
-// //tämä on EPÄPYHÄ HELVETTI ja aion korjata valojen toimivuuen lol. oletan et se on jotai PlayerPrefsin kanssa,
-// //koska ne valot sai pois päältä ainoastaa jos sen asetuksen laitto päälle ja pois. like seriously wtf
-// //HUOM. tein pieniä muutoksia joten jos ne ei toimi nii tuun korjaa tulevina päivinä, oletan vaan kaiken :)))
-// public class ColorChanger : MonoBehaviour
-// {
-//     public Light pointLight;
-//     public Light right;
-//     public Light left;
+//tämä on EPÄPYHÄ HELVETTI ja aion korjata valojen toimivuuen lol. oletan et se on jotai PlayerPrefsin kanssa,
+//koska ne valot sai pois päältä ainoastaa jos sen asetuksen laitto päälle ja pois. like seriously wtf
+//HUOM. tein pieniä muutoksia joten jos ne ei toimi nii tuun korjaa tulevina päivinä, oletan vaan kaiken :)))
+public class ColorChanger : MonoBehaviour
+{
+    public Light pointLight;
+    public Light right;
+    public Light left;
 
-//     public float duration = 1.0f;
+    public float duration = 1.0f;
 
-//     void Awake()
-//     {
-//         Controls = new CarInputActions();
-//         Controls.Enable();
-//     }
+    void Awake()
+    {
+        Controls = new CarInputActions();
+        Controls.Enable();
+    }
 
-//     CarInputActions Controls;
-
-
-
-//     private void OnEnable()
-//     {
-//         Controls.Enable();
-        
-//         foreach (Transform child in transform)
-//         {
-//             if (child.CompareTag("pl"))
-//             {
-//                 if (childLight != null)
-//                 {
-//                     pointLight = childLight;
-//                 }
-//             }
-//             else if (child.CompareTag("rl"))
-//             {
-//                 if (childLight != null)
-//                 {
-//                     right = childLight;
-//                 }
-//             }
-//             else if (child.CompareTag("ll"))
-//             {
-//                 if (childLight != null)
-//                 {
-//                     left = childLight;
-//                 }
-//             }
-            
-//             Light childLight = child.GetComponent<Light>();
-//         }
-//     }
-
-//     private void OnDisable()
-//     {
-//         Controls.Disable();
-//     }
+    CarInputActions Controls;
 
 
 
-//     private void Update()
-//     {
-//         if (CheckLightState() == true)
-//         {
-//             if (Controls.CarControls.lights.triggered)
-//             {
-//                 left.enabled = !left.enabled;
-//                 right.enabled = !right.enabled;
-//             }
+    private void OnEnable()
+    {
+        Controls.Enable();
+    
+        foreach (Transform child in transform)
+        {
+            Light childLight = child.GetComponent<Light>();
 
-//             if (Controls.CarControls.underglow.triggered)
-//             {
-//                 pointLight.enabled = !pointLight.enabled;
-//             }
-//         }
+            if (child.CompareTag("pl"))
+            {
+                if (childLight != null)
+                {
+                    pointLight = childLight;
+                }
+            }
+            else if (child.CompareTag("rl"))
+            {
+                if (childLight != null)
+                {
+                    right = childLight;
+                }
+            }
+            else if (child.CompareTag("ll"))
+            {
+                if (childLight != null)
+                {
+                    left = childLight;
+                }
+            }
+        }
+    }
 
-//         if (pointLight.enabled)
-//         {
-//             float t = Mathf.PingPong(Time.time / duration, 1.0f);
-//             pointLight.color = Color.Lerp(Color.red, Color.blue, t);
-//         }
-//     }
+    private void OnDisable()
+    {
+        Controls.Disable();
+    }
 
-//     public void CheckLightState()
-//     {
-//         bool lightEnabled = PlayerPrefs.GetInt("optionTest_value") == 0;
-//         if (!lightEnabled)
-//         {
-//             pointLight.enabled = false;
-//             left.enabled = false;
-//             right.enabled = false;
-//         }
-//         else
-//         {
-//             pointLight.enabled = true;
-//             left.enabled = true;
-//             right.enabled = true;
-//         }
-//         return lightEnabled;
-//     }
-// }
+
+
+    private void Update()
+    {
+        if (CheckLightState() == true)
+        {
+            if (Controls.CarControls.lights.triggered)
+            {
+                left.enabled = !left.enabled;
+                right.enabled = !right.enabled;
+            }
+
+            if (Controls.CarControls.underglow.triggered)
+            {
+                pointLight.enabled = !pointLight.enabled;
+            }
+        }
+
+        if (pointLight.enabled)
+        {
+            float t = Mathf.PingPong(Time.time / duration, 1.0f);
+            pointLight.color = Color.Lerp(Color.red, Color.blue, t);
+        }
+    }
+
+    public bool CheckLightState()
+    {
+        bool lightsEnabled = PlayerPrefs.GetInt("optionTest_value") == 0;
+        pointLight.enabled = lightsEnabled;
+        left.enabled = lightsEnabled;
+        right.enabled = lightsEnabled;
+
+        return lightsEnabled;
+    }
+}
