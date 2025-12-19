81d0a79f113646e17759d806856efbe58ab53b42
ThatOneGuyC <thatoneguyextra@gmail.com>
2025-12-19 15:02:12 +0200

    bad word detection without regex feat lamelemon

--- Assets/scripts/userDataInput.cs
+++ Assets/scripts/userDataInput.cs
@@ -3,36 +3,83 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 public class userDataInput : MonoBehaviour
 {
     private TMP_InputField inputField;
     public string userName;
     private TextAsset jsonText;
+    private TextAsset jsonRegexText;
     private string[] bannedNamesArray;
     private string[] bannedNamePopups;
     [SerializeField] private Text bannedPopup;
     private Button enter;
     private RacerScript racerscript;
+    private HashSet<string> bannedRegexWords;
 
     void OnEnable()
     {
         inputField = GameObject.Find("userDataInput").GetComponent<TMP_InputField>();
         enter = GameObject.Find("Enter").GetComponent<Button>();
+
         jsonText = Resources.Load<TextAsset>("bannedNames");
         bannedNamesArray = JsonUtility.FromJson<BannedNames>(jsonText.text).names.ToArray();
+        jsonRegexText = Resources.Load<TextAsset>("regexReplacementValues");
+        Dictionary<char, string> regexReplacements = JsonUtility.FromJson<Dictionary<char, string>>(jsonRegexText.text);
         bannedNamePopups = new string[]
         {
             "Name cannot be empty!",
             "Invalid name!"
         };
+
         racerscript = FindFirstObjectByType<RacerScript>();
+
+        bannedRegexWords = AddRegexToHashset(bannedNamesArray, regexReplacements);
     }
     
     [Serializable]
     public class BannedNames
     {
         public string[] names;
+    }
+
+    /// <summary>
+    /// ota lista kielletyistä sanoista (bannedWords) ja lisää sille maholliset kirjainten korvaukset (replacements).
+    /// </summary>
+    /// <param name="bannedWords">array tuhmista sanoista >:(</param>
+    /// <param name="replacements">dictionary mahollisista korvauksista, esim. "b":"[83]"</param>
+    /// <returns>hashset string lol</returns>
+    
+    // kiitos lamelemon
+    private HashSet<string> AddRegexToHashset(string[] bannedWords, Dictionary<char, string> replacements)
+    {
+        HashSet<string> bannedRegex = new();
+
+        foreach (string word in bannedWords)
+        {
+            string regexWord = "";
+            foreach (char letter in word)
+            {
+                //tämä ei kuitenkaa toimi
+                if (replacements.Keys.Contains(letter))
+                {
+                    Debug.Log("kyllä on!");
+                    regexWord += replacements[letter];
+                }
+                else
+                {
+                    regexWord += letter;
+                }
+            }
+            //lisää pahat sanat pahaan paikkaan
+            bannedRegex.Add(regexWord);
+        }
+
+        foreach (string entrything in bannedRegex)
+            Debug.Log($"THIS IS A BAD NAME!!! {entrything}");
+        return bannedRegex;
     }
 
     public void UpdateUserName()
@@ -43,14 +90,20 @@ public class userDataInput : MonoBehaviour
 
     public void CheckForInvalidName()
     {
+        foreach (string bannedName in bannedRegexWords)
+        {
+            //huom. tää jo toimii
+            if (Regex.IsMatch(userName, bannedName))
+            {
+                Debug.Log("THIS IS A BAD NAME!!!");
+                bannedPopup.text = bannedNamePopups[1];
+                enter.interactable = false;
+                return;
+            }
+        }
         if (userName.Length == 0)
         {
             bannedPopup.text = bannedNamePopups[0];
-            enter.interactable = false;
-        }
-        else if (bannedNamesArray.Contains(userName.ToLower()))
-        {
-            bannedPopup.text = bannedNamePopups[1];
             enter.interactable = false;
         }
         else
