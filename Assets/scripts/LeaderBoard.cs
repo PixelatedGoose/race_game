ad30b7db143d72b2ce6cbf8e919a519886087962
ThatOneGuyC <thatoneguyextra@gmail.com>
2025-12-24 03:54:04 +0200

    Or you can be a Grinch (grunch)

--- Assets/scripts/LeaderBoard.cs
+++ Assets/scripts/LeaderBoard.cs
@@ -3,6 +3,8 @@ using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
+using UnityEngine.UI;
 
 public class LeaderBoard : MonoBehaviour
 {
@@ -11,11 +13,13 @@ public class LeaderBoard : MonoBehaviour
     [SerializeField] private TextMeshProUGUI[] timeTexts = new TextMeshProUGUI[5];
     [SerializeField] private TextMeshProUGUI[] scoreTexts = new TextMeshProUGUI[5];
     [SerializeField] private TextMeshProUGUI[] mapTexts = new TextMeshProUGUI[5];
+    [SerializeField] private TextMeshProUGUI[] carTexts = new TextMeshProUGUI[5];
 
     [Header("Settings")]
     [SerializeField] private float updateInterval = 7f;
     [SerializeField] private bool showMapColumn = true;
     [SerializeField] private bool showScoreColumn = true;
+    [SerializeField] private bool loadCar = false;
 
     private RaceResultHandler resultHandler;
     private Coroutine updateCoroutine;
@@ -25,6 +29,14 @@ public class LeaderBoard : MonoBehaviour
         resultHandler = new RaceResultHandler(Application.persistentDataPath, "race_result.json");
         UpdateLeaderboard();
         updateCoroutine = StartCoroutine(UpdateLeaderboardRoutine());
+
+        if (loadCar)
+        {
+            foreach (TextMeshProUGUI text in carTexts)
+            {
+                text.gameObject.SetActive(true);
+            }
+        }
     }
 
     void OnDestroy()
@@ -93,6 +105,11 @@ public class LeaderBoard : MonoBehaviour
         {
             mapTexts[index].text = result.map;
         }
+
+        if (loadCar)
+        {
+            carTexts[index].text = result.carName;
+        }
     }
 
     private void ClearSlot(int index)
@@ -115,17 +132,6 @@ public class LeaderBoard : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             ClearSlot(i);
-        }
-    }
-
-    private string GetPositionSuffix(int position)
-    {
-        switch (position)
-        {
-            case 1: return "1st";
-            case 2: return "2nd";
-            case 3: return "3rd";
-            default: return position + "th";
         }
     }
 }
