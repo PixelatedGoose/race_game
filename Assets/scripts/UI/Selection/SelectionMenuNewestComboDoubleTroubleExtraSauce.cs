1b4e0599c6b269d5cf9e172d4cee775fd46d653d
ThatOneGuyC <thatoneguyextra@gmail.com>
2026-02-04 03:27:18 +0200

    selection menu 2½: the selectioning

--- Assets/scripts/UI/Selection/SelectionMenuNewestComboDoubleTroubleExtraSauce.cs
+++ Assets/scripts/UI/Selection/SelectionMenuNewestComboDoubleTroubleExtraSauce.cs
@@ -5,15 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 using System.Collections.Generic;
-using System.Reflection.Emit;
 using Newtonsoft.Json;
 using System.Linq;
 
 public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
 {
-    public GameObject carSelectionO;
-    public GameObject mapSelectionO;
-    public GameObject optionSelection;
     public GameObject[] msObjectsList;
     private float schizophrenia;
     private GameObject loadObjects;
@@ -28,6 +24,12 @@ public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
     private Dictionary<string, Dictionary<string, string>> details;
     [SerializeField] private TextMeshProUGUI detailsPanelText;
 
+    [SerializeField] private int selectionIndex = 0;
+    [SerializeField] private GameObject[] selectionMenus;
+    [SerializeField] private GameObject detailsPanel;
+
+
+
     void Awake()
     {
         selectionDetails = Resources.Load<TextAsset>("selectionDetails");
@@ -36,13 +38,16 @@ public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
         /* selectText = GameObject.Find("SelectYoMap").GetComponent<Text>();
         //pitää ettiä tekstit jollai array tavalla
         scoreText = GameObject.Find("ScoreOnThaAuto").GetComponent<Text>();
-        carSelectionO = GameObject.Find("CarSelectionNew");
-        mapSelectionO = GameObject.Find("mapSelectionObj");
         loadObjects = GameObject.Find("loadObjects");
         msObjectsList = GameObject.FindGameObjectsWithTag("msObj");
         loadingLoop = GameObject.Find("loadingLoop").GetComponent<AudioSource>(); */
 
-        //mfw kun pitää siirtää koko maps paska car selection scriptiin
+        selectionMenus = GameObject.FindGameObjectsWithTag("selectionMenu")
+        .OrderBy(go => go.name).ToArray();
+        selectionMenus[1].SetActive(false);
+        selectionMenus[2].SetActive(false);
+
+        //detailsPanel = GameObject.Find("detailsPanel");
     }
 
     public void MapFallAnimResetPos()
@@ -59,31 +64,22 @@ public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
         scoreText.rectTransform.anchoredPosition = new Vector2(520.0f, scoreText.rectTransform.anchoredPosition.y);
     }
 
-    /// <summary>
-    /// käytetään mapin valintaan. ottaa mapin PlayerPrefsistä
-    /// </summary>
-    /// <param name="selecta">PERUS mappi, jonka haluat ladata</param>
-    public void MapButtonPress(string selecta)
-    {
-        string selectedMapTrue;
-        if (AItoggle.isOn)
-            selectedMapTrue = $"ai_{selecta}";
-        else
-            selectedMapTrue = selecta;
-        PlayerPrefs.SetString("SelectedMap", selectedMapTrue);
-        PlayerPrefs.Save();
-        Debug.Log($"onnittelut, voitit lomamatkan kohteeseen: {selectedMapTrue}");
+    //selection > singleplayer, ai botit, multiplayer
+    //ai botit on sama ku singleplayer + ai bottien säätö selection (mihi se menee?)
+    //tätä ekaa valintaa käytetään määrittämään arrayn koko
+    //(paitsi multiplayer koska se on eri scene)
 
-        StartCoroutine(MapButtonFunc());
-    }
+    //map > car > ai options (?) > options > gaming singleplayeris
+    //uus scene: lobby > map > car > options > gaming multiplayeris
 
     //helpottaa asioit ja se on coroutine jo valmiiksi
+    //poistan turhan methodin myöhemmin
     public void MapFallAnim()
     {
-        StartCoroutine(MapFallAnimFunc());
+        MapFallAnimFunc();
     }
 
-    private IEnumerator MapButtonFunc()
+    private IEnumerator MapButtonPress()
     {
         loadingLoop.Play();
 
@@ -100,7 +96,7 @@ public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
         SceneManager.LoadSceneAsync(PlayerPrefs.GetString("SelectedMap"));
     }
 
-    private IEnumerator MapFallAnimFunc()
+    private void MapFallAnimFunc()
     {
         LeanTween.value(selectText.gameObject, selectText.rectTransform.anchoredPosition.x, -20.0f, 2f)
             .setEase(LeanTweenType.easeOutExpo)
@@ -115,60 +111,74 @@ public class SelectionMenuNewestComboDoubleTroubleExtraSauce : MonoBehaviour
             {
             scoreText.rectTransform.anchoredPosition = new Vector2(val, scoreText.rectTransform.anchoredPosition.y);
             });
-
-        foreach (GameObject mapImg in maps)
-        {
-            RectTransform rectTransform = mapImg.GetComponent<RectTransform>();
-
-            //LeanTween.value on ainoa tapa muuttaa Rect Transformin y:tä
-            LeanTween.value(mapImg, rectTransform.anchoredPosition.y, -60.0f, 0.7f)
-            .setEase(LeanTweenType.easeInOutExpo)
-            .setOnUpdate((float val) =>
-            {
-                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, val);
-            });
-            yield return new WaitForSeconds(0.3f);
-        }
     }
 
     private void Update()
     {
         //tän kaiken pitää executtaa AINOASTAAN kun se vaihtuu jos se on helppoa
         GameObject current = EventSystem.current.currentSelectedGameObject;
-        Debug.LogWarning(current);
-        
+        //Debug.LogWarning(current);
+
         if (current != null)
         {
-            if (details["options"].ContainsKey(current.name))
-                detailsPanelText.text = details["options"][current.name];
+            //vuoden indeksoinnit siitä
+            if (details[selectionMenus[selectionIndex].name].ContainsKey(current.name))
+                detailsPanelText.text = details[selectionMenus[selectionIndex].name][current.name];
+            //säilytä edellinen teksti details ruudus jos dropdown on valittuna
             else if (current.name.StartsWith("Item"))
-                Debug.Log("do nothing lol");
+                return;
             else
                 detailsPanelText.text = "";
         }
     }
 
-    //tää pitää muuttaa arrayta tai vastaavaa käyttäväks, joka sisältää:
     /* 1. map, 2. car, 3. settings, 4. loading :))))
     vaihtaa sit indeksien mukaan tota
-
-    HUOM. alemman buttonin teksti pitää vaihtaa "exit" kun on ekassa osassa
-    ja ylemmän buttonin teksti pitää vaihtaa "go!" tai jtn samanlaista */
-    public void ActivateMapSelection()
+    HUOM. ylemmän buttonin teksti vois vaihtaa "go!" kun on viimeses osas */
+    public void Next()
     {
-        carSelectionO.SetActive(false);
-        mapSelectionO.SetActive(true);
+        selectionIndex++;
+        selectionMenus[selectionIndex].SetActive(true);
+        selectionMenus[selectionIndex - 1].SetActive(false);
+    }
+    public void Back()
+    {
+        //normal back
+        if (selectionIndex != 0)
+        {
+            selectionIndex--;
+            selectionMenus[selectionIndex].SetActive(true);
+            selectionMenus[selectionIndex + 1].SetActive(false);
+            //jos index == 0 mentyään takasin
+            if (selectionIndex == 0)
+            {
+                detailsPanel.SetActive(false);
+            }
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync("MainMenu");
+        }
     }
 
-    /* public void Back()
+    /// <summary>
+    /// WIP
+    /// </summary>
+    /// <param name="selecta">WIP</param>
+    private void SetMapToLoad(string selecta)
     {
-        MapFallAnimResetPos();
-        carSelectionO.SetActive(true);
-        mapSelectionO.SetActive(false);
-    } */
+        string selectedMap;
+        if (AItoggle.isOn)
+            selectedMap = $"ai_{selecta}";
+        else
+            selectedMap = selecta;
+        //if (dropdown == night)
+            //selectedmap += $"_night"
+        //else
+            //nothing; valmiiks laitettu jo
 
-    public void BackToMainMenu()
-    {
-        SceneManager.LoadSceneAsync("MainMenu");
+        PlayerPrefs.SetString("SelectedMap", selectedMap);
+        PlayerPrefs.Save();
+        Debug.Log($"onnittelut, voitat lomamatkan kohteeseen: {selectedMap}");
     }
 }
