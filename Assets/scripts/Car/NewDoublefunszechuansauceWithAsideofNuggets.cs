683830e1c3d8d435077d67b94384b119fcd3b7e5
Vizl87 <ojantakanenaleksi@gmail.com>
2026-05-06 12:24:19 +0300

    merge conflict my beloved

--- Assets/scripts/Car/NewDoublefunszechuansauceWithAsideofNuggets.cs
+++ Assets/scripts/Car/NewDoublefunszechuansauceWithAsideofNuggets.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
@@ -7,16 +7,16 @@ using UnityEngine.UI;
 public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
 {
     public CarInputActions Controls { get; protected set; }
-    RacerScript racerScript;
     LogitechMovement LGM;
     private string CurrentControlScheme;
     PlayerInput PlayerInput;
     [SerializeField] private GameObject carLights;
     private Material carLightsMaterial;
+    public Material PixelCount;
 
     float DriftDirection;
     float DriftTurnSpeed = 5f;
-    [SerializeField] float DriftTurnSpeedAtHighSpeed = 2f;
+    [SerializeField] float DriftTurn = 2f;
     float driftMultiplier = 1.2f;
     float SteerDeadzone = 0.2f;
     float CurrentDriftAngle = 0f;
@@ -24,6 +24,7 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
     float RightDriftAngle = 45f;
     float LeftDriftAngle = -45f;
 
+    float GetSteerSign(float steer) => Mathf.Abs(steer) > SteerDeadzone ? Mathf.Sign(steer) : 0f;
 
 
 
@@ -32,7 +33,6 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
         Controls = new CarInputActions();
         CarRb = GetComponent<Rigidbody>();
         TurbeBar = GameManager.instance.CarUI.transform.Find("TurbeDisplay").GetComponentInChildren<Image>();
-        racerScript = GetComponent<RacerScript>();
         TryGetComponent(out LGM);
         
         Controls.Enable();
@@ -73,8 +73,8 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
         Controls.CarControls.Move.performed += OnMovePerformed;
         Controls.CarControls.Move.canceled  += OnMoveCanceled;
 
-        Controls.CarControls.Drift.performed += OnDriftPerformed;
-        Controls.CarControls.Drift.canceled  += OnDriftCanceled;
+        Controls.CarControls.Drift.performed   += OnDriftPerformed;
+        Controls.CarControls.Drift.canceled    += OnDriftCanceled;
         Controls.CarControls.Brake.performed += OnBrakePerformed;
         Controls.CarControls.Brake.canceled  += OnBrakeCanceled;
 
@@ -132,10 +132,9 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
     override protected void Start()
     {
         base.Start();
-        
     }
 
-    
+    //movement or anykind of input related will go here
     protected void Update()
     {
         MovementInputs = Controls.CarControls.Move.ReadValue<Vector2>();
@@ -143,8 +142,8 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
 
         Steer();
         CarMovement();
-        ApplyDriftTurn();
-        WheelEffects(IsDrifting);
+        ApplySpeedLimit();
+        Decelerate();
     }
 
     //physics related will go here
@@ -163,42 +162,8 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
 //         TurbeMeter();
 //     }
 
-    void GetInputs()
-    {
-        Vector2 move = Controls.CarControls.Move.ReadValue<Vector2>();
-        SteerInput = move.x;
-        MoveInput = move.y;
-    }
-
-    float GetDriftSteer()
-    {
-        if (!IsDrifting)
-            return SteerInput;
-
-        if (Mathf.Approximately(DriftDirection, 0f))
-            DriftDirection = GetSteerSign(SteerInput);
 
 
-        return DriftDirection;
-    }
-
-    void ApplyDriftTurn()
-    {
-        if (!IsDrifting)
-            return;
-        
-        float speedRatio = Mathf.Clamp01(CarRb.linearVelocity.magnitude / Mathf.Max(MaxSpeed, 0.01f));
-        float turnSpeed = Mathf.Lerp(DriftTurnSpeed, DriftTurnSpeedAtHighSpeed, speedRatio);
-
-        float deltaTurn = DriftDirection * turnSpeed * driftMultiplier * Time.deltaTime;
-
-        float newAngle = Mathf.Clamp(CurrentDriftAngle + deltaTurn, LeftDriftAngle, RightDriftAngle);
-        float appliedTurn = newAngle - CurrentDriftAngle;
-
-        CurrentDriftAngle = newAngle;
-
-        transform.Rotate(0f, appliedTurn, 0f);
-    }
 
 
     //Arcade car style movement
@@ -207,12 +172,18 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
         Vector3 flatForwardVelocity = 
         transform.forward * Mathf.MoveTowards(
             CarRb.linearVelocity.magnitude,
-            MaxSpeed * Mathf.Abs(MovementInputs.y), 
+            MaxSpeed * Mathf.Sign(MovementInputs.y), 
             Acceleration * Time.deltaTime
         );
         flatForwardVelocity.y = CarRb.linearVelocity.y;
         CarRb.linearVelocity = flatForwardVelocity;
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
 
     void OnBrakePerformed(InputAction.CallbackContext ctx)
     {
@@ -230,12 +201,27 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
         }
     }
 
+    void ApplyDriftTurn()
+    {
+        if (!IsDrifting) return;
+
+        
+        float speedRatio = Mathf.Clamp01(CarRb.linearVelocity.magnitude / Mathf.Max(MaxSpeed, 0.01f));
+
+        float newAngle = Mathf.Clamp(CurrentDriftAngle + DriftDirection * Mathf.Lerp(DriftTurnSpeed, DriftTurn, speedRatio) * driftMultiplier * Time.deltaTime, LeftDriftAngle, RightDriftAngle);
+
+        CurrentDriftAngle = newAngle;
+
+        transform.Rotate(0f, newAngle - CurrentDriftAngle, 0f);
+    }
+
 
     // your car is drifting bro better go and call your insurance company
     void OnDriftPerformed(InputAction.CallbackContext ctx)
     {
         IsDrifting = true;
         DriftDirection = 0f;
+        ApplyDriftTurn();
         WheelEffects(true);
     }
 
@@ -246,5 +232,4 @@ public class NewDoublefunszechuansauceWithAsideofNuggets : BaseCarController
         WheelEffects(false);
     }
 
-    float GetSteerSign(float steer) => Mathf.Abs(steer) > SteerDeadzone ? Mathf.Sign(steer) : 0f;
 }
